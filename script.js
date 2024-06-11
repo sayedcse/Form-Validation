d3d016cae3c49a74fb5be6912f6592762e79cb34
sayedcse <saidursayed1@gmail.com>
2024-06-12 04:05:05 +0600

    validation updated project done

--- script.js
+++ script.js
@@ -18,10 +18,14 @@ function showError(input, message) {
 }
 
 // Email Validation
-function isValidEmail(EMAIL) {
+function checkEmail(input) {
     const RE =
         /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    return RE.test(String(EMAIL).toLowerCase());
+    if (RE.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Enter a valid Email');
+    }
 }
 
 //Check required fieldset
@@ -29,8 +33,35 @@ function checkRequired(inputArr) {
     inputArr.forEach(function (input) {
         if (input.value.trim() == '') {
             showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
         }
     });
+}
+
+//Check input Length
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(
+            input,
+            `${getFieldName(input)} must be at lest ${min} characters`
+        );
+    } else if (input.value.length > max) {
+        showError(
+            input,
+            `${getFieldName(input)} must be less than ${max} characters`
+        );
+    } else {
+        showSuccess(input);
+    }
+}
+
+// check Password match
+
+function checkPassMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, 'Password do not match');
+    }
 }
 
 //get filed name
@@ -43,4 +74,8 @@ FORM.addEventListener('submit', function (e) {
     e.preventDefault();
 
     checkRequired([USER_NAME, EMAIL, PASSWORD, PASSWORD_2]);
+    checkLength(USER_NAME, 3, 15);
+    checkLength(PASSWORD, 4, 16);
+    checkEmail(EMAIL);
+    checkPassMatch(PASSWORD, PASSWORD_2);
 });
